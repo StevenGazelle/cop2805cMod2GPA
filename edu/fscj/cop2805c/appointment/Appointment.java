71c7aecb0f90c01469c86a9adf11f58f6fa2b916
StevenGazelle <s3068106@students.fscj.edu>
2023-01-27 09:11:28 -0500

    Appointment and Contact class with ZonedDateTime

---
+++ edu/fscj/cop2805c/appointment/Appointment.java
@@ -0,0 +1,85 @@
+//Appointment.java
+//Steven Gsell
+//1/26/2023
+//Program that displays appointment information
+package edu.fscj.cop2805c.appointment;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+enum REMINDER {NONE, TEXT, EMAIL};
+
+public class Appointment {
+    //fields
+    String title, description;
+    Contact contact;
+    ZonedDateTime appointmentTime, reminderTime;
+
+    //constructors
+    public Appointment(String title,
+                       String description,
+                       Contact contact,
+                       ZonedDateTime appointmentTime){
+        this.title = title;
+        this.description = description;
+        this.contact = contact;
+        this.appointmentTime = appointmentTime;
+        this.reminderTime = appointmentTime.minusHours(12);
+    }
+
+    //methods
+    @Override
+    public String toString(){
+        String retString = "Your current appointment:\n";
+        retString += ("\tTitle: " + title + "\n" +
+                "\tDescription: " + description + "\n" +
+                "\tContact: " + contact + "\n" +
+                "\tAppt Date/Time: " + appointmentTime + "\n" +
+                "\tReminder: " + reminderTime);
+
+        return retString;
+    }
+    //main
+    public static void main(String[] args){
+        Contact apptContact = new Contact(new StringBuilder("Gsell, Steven"),
+                "StevenGsell@email.com", "904-555-5555",
+                ZoneId.systemDefault(), REMINDER.TEXT);
+
+        Appointment appt = new Appointment("Test Appointment",
+                "This is a test appointment", apptContact,
+                ZonedDateTime.now().plusWeeks(2));
+
+        System.out.println(appt);
+    }
+}
+
+class Contact {
+    //fields
+    private StringBuilder name;
+    private String email, phoneNumber;
+    private ZoneId timeZone;
+    private REMINDER reminderPref;
+
+    //constructors
+    public Contact(StringBuilder name, String email, String phoneNumber,
+                   ZoneId timeZone, REMINDER reminderPref){
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.timeZone = timeZone;
+        this.reminderPref = reminderPref;
+    }
+
+    //methods
+    @Override
+    public String toString() {
+        String retString = "";
+        retString += (email + ",(" + name + ")," +
+                    phoneNumber + "," + reminderPref.name() +
+                    "," + timeZone);
+
+        return retString;
+    }
+}
